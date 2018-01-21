67958537a4068c9108fbca7432d5e7e58d5686dc
Victor <victor.ostertag@gmail.com>
2018-01-21 02:22:35 +0000

    Create service_lane.py

---
+++ algorithms/implementation/service_lane.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+
+import sys
+
+
+n,t = input().strip().split(' ')
+n,t = [int(n),int(t)]
+width = [int(width_temp) for width_temp in input().strip().split(' ')]
+for a0 in range(t):
+    i,j = input().strip().split(' ')
+    i,j = [int(i),int(j)]
+    sizes = []
+    for x in range(i, j+1):
+        sizes.append(width[x])
+    print(min(sizes))
