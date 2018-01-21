8c122b61944a069c7e90daef89f7d8200f73455b
Victor <victor.ostertag@gmail.com>
2018-01-21 02:00:58 +0000

    Update migratory_birds.py

--- algorithms/implementation/migratory_birds.py
+++ algorithms/implementation/migratory_birds.py
@@ -1,1 +1,16 @@
+#!/bin/python3
 
+import sys
+
+def migratoryBirds(n, ar):
+    number = [0,0,0,0,0]
+    for i in range(n):
+        number[ar[i]-1] += 1
+        
+    return number.index(max(number))+1
+            
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = migratoryBirds(n, ar)
+print(result)
